71912689cdbc827a698780f518faaf86f064a3de
Chris Linstid <clinstid@gmail.com>
2013-09-09 01:40:16 +0000

    Update collision detection to work with android touch.

--- shape_monster.js
+++ shape_monster.js
@@ -11,6 +11,26 @@
 // monsters that feed on them. There really is no end to the game, it's just
 // for fun.
 
+// get coordinates for the end result of a touch/click and drag
+// returns an array: [x, y]
+function getDragCoordinates(dragEvent) {
+    var coordinates = [-1, -1];
+    if (dragEvent instanceof TouchEvent) {
+        coordinates[0] = dragEvent.targetNode.attrs.x;
+        coordinates[1] = dragEvent.targetNode.attrs.y;
+    }
+    else if (dragEvent instanceof MouseEvent) {
+        coordinates[0] = dragEvent.layerX;
+        coordinates[1] = dragEvent.layerY;
+    }
+    else
+    {
+        return null;
+    }
+
+    return coordinates;
+}
+
 // document has loaded, let's get those shape monsters up and running
 $(document).ready(function() {
     var stage = new Kinetic.Stage({
@@ -64,23 +84,6 @@ $(document).ready(function() {
         draggable: true
     });
 
-    rect.on('dragmove', function(mouseEvent) {
-        rect.moveToTop();
-        if (rectMonster.intersects([mouseEvent.layerX,
-                                   mouseEvent.layerY])) {
-            var tween = new Kinetic.Tween({
-                node: rect,
-                duration: 0.5,
-                width: 0,
-                height: 0
-            });
-            tween.play();
-            window.setTimeout(500, function() {
-                rect.hide();
-            });
-        }
-    });
-
     var circle = new Kinetic.Circle({
         name: 'circle',
         x: 200,
@@ -90,22 +93,6 @@ $(document).ready(function() {
         stroke: 'black',
         strokeWidth: 2,
         draggable: true
-    });
-
-    circle.on('dragmove', function(mouseEvent) {
-        circle.moveToTop();
-        if (circleMonster.intersects([mouseEvent.layerX,
-                                     mouseEvent.layerY])) {
-            var tween = new Kinetic.Tween({
-                node: circle,
-                duration: 0.5,
-                radius: 0,
-            });
-            tween.play();
-            window.setTimeout(500, function() {
-                circle.hide();
-            });
-        }
     });
 
     var triangle = new Kinetic.RegularPolygon({
@@ -120,22 +107,6 @@ $(document).ready(function() {
         draggable: true
     });
 
-    triangle.on('dragmove', function(mouseEvent) {
-        triangle.moveToTop();
-        if (triangleMonster.intersects([mouseEvent.layerX,
-                                       mouseEvent.layerY])) {
-            var tween = new Kinetic.Tween({
-                node: triangle,
-                duration: 0.5,
-                radius: 0,
-            });
-            tween.play();
-            window.setTimeout(500, function() {
-                triangle.hide();
-            });
-        }
-    });
-
     var monsterLayer = new Kinetic.Layer();
     monsterLayer.add(rectMonster);
     monsterLayer.add(circleMonster);
@@ -147,5 +118,75 @@ $(document).ready(function() {
     shapeLayer.add(circle);
     shapeLayer.add(triangle);
     stage.add(shapeLayer);
+
+    rect.on('dragmove', function(dragEvent) {
+        rect.moveToTop();
+
+        var coordinates = getDragCoordinates(dragEvent);
+        if (!coordinates) {
+            return;
+        }
+        var intersectionObject = monsterLayer.getIntersection(coordinates);
+        if (intersectionObject && intersectionObject.shape === rectMonster) {
+            var tween = new Kinetic.Tween({
+                node: rect,
+                duration: 0.5,
+                width: 0,
+                height: 0
+            });
+            rect.setDraggable(false);
+            tween.play();
+            window.setTimeout(500, function() {
+                rect.remove();
+            });
+        }
+    });
+
+    circle.on('dragmove', function(dragEvent) {
+        circle.moveToTop();
+
+        var coordinates = getDragCoordinates(dragEvent);
+        if (!coordinates) {
+            return;
+        }
+
+        var intersectionObject = monsterLayer.getIntersection(coordinates);
+        if (intersectionObject && intersectionObject.shape === circleMonster) {
+            var tween = new Kinetic.Tween({
+                node: circle,
+                duration: 0.5,
+                radius: 0,
+            });
+            circle.stopDrag();
+            tween.play();
+            window.setTimeout(500, function() {
+                circle.remove();
+            });
+        }
+    });
+
+    triangle.on('dragmove', function(dragEvent) {
+        triangle.moveToTop();
+
+        var coordinates = getDragCoordinates(dragEvent);
+        if (!coordinates) {
+            return;
+        }
+
+        var intersectionObject = monsterLayer.getIntersection(coordinates);
+        if (intersectionObject && intersectionObject.shape === triangleMonster) {
+            var tween = new Kinetic.Tween({
+                node: triangle,
+                duration: 0.5,
+                radius: 0,
+            });
+            triangle.stopDrag();
+            tween.play();
+            window.setTimeout(500, function() {
+                triangle.remove();
+            });
+        }
+    });
+
 });
 
